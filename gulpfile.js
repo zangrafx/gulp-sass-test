649e0e34528f0dd8d6d90cb56f64a457380c6e4b
zangrafx <zangrafx@gmail.com>
2015-07-24 23:02:32 +0200

    add browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,34 @@
 
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var path = require("path");
 var sourcemaps = require("gulp-sourcemaps");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer-core");
+var browserSync = require("browser-sync").create();
+
+// Static Server + watching scss/html files
+gulp.task("serve", ["sass"], function () {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("sass/**/*.scss", ["sass"]);
+    gulp.watch("index.html").on("change", browserSync.reload);
+});
 
 gulp.task("sass", function () {
     var processors = [
         autoprefixer({browsers: ["last 3 versions", "ie >= 8", "android >= 2.3.3"]})
     ];
-    gulp.src("./scss/**/*.scss")
+    gulp.src(path.join(__dirname, "/sass/**/*.scss"))
         .pipe(sourcemaps.init())
         .pipe(sass().on("error", sass.logError))
         .pipe(postcss(processors))
         .pipe(sourcemaps.write("./maps"))
-        .pipe(gulp.dest("./css"));
+        .pipe(gulp.dest(path.join(__dirname, "/css/")))
+        .pipe(browserSync.stream({match: "**/*.css"}));
 });
 
 gulp.task("sass:watch", function () {
